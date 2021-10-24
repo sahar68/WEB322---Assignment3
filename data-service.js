eba0175f7d70ca354e78f63c904bd763647541e5
Sahar <shosseinichegeni@myseneca.ca>
2021-10-24 19:45:29 -0400

    last commit

--- data-service.js
+++ data-service.js
@@ -4,6 +4,109 @@ var employees = [];
 var departments = [];
 
 //*****************************************************************/
+module.exports.addEmployee = function (employeeData) {
+
+    var promise = new Promise((resolve, reject) => {
+        
+       if (typeof employeeData.isManager === "undefined") {
+            employeeData.isManager = false;
+       } 
+       else {
+            employeeData.isManager = true;
+       }
+
+       employeeData.employeeNum = employees.length + 1;
+       employees.push(employeeData);
+        
+     resolve (employees);
+     })
+     return promise;
+
+};
+//*****************************************************************/
+
+module.exports.getEmployeesByStatus = function (statusId) {
+    var emp = [];
+    var promise = new Promise((resolve, reject) => {
+      
+       for (var i=0; i < employees.length; i++){
+           if (employees[i].status == statusId) {
+               emp.push(employees[i]);
+           }
+       }
+       if(emp.length === 0) {
+        var err = "getEmployeesByStatus() Not Filled.";
+        reject({message: err});
+       }  
+    resolve (emp);
+    })
+    return promise;
+
+};
+//*****************************************************************/
+
+module.exports.getEmployeesByDepartment = function (departmentId) {
+
+    var emp = [];
+    var promise = new Promise((resolve, reject) => {
+      
+       for (var i=0; i < employees.length; i++){
+           if (employees[i].department == departmentId) {
+               emp.push(employees[i]);
+           }
+       }
+       if(emp.length === 0) {
+        var err = "getEmployeesByDepartment() Not Filled.";
+        reject({message: err});
+       }  
+    resolve (emp);
+    })
+    return promise;
+
+};
+//*****************************************************************/
+
+module.exports.getEmployeesByManager = function (managerBool) {
+   
+    var emp = [];
+    var promise = new Promise((resolve, reject) => {
+       for (var i=0; i < employees.length; i++){
+           if (employees[i].isManager == managerBool) {
+               emp.push(employees[i]);
+           }
+       }
+       if(emp.length === 0) {
+        var err = "getEmployeesByManager() Not Filled.";
+        reject({message: err});
+       }  
+
+    resolve (emp);
+    })
+    return promise;
+
+};
+//*****************************************************************/
+
+module.exports.getEmployeeByNum = function (num) {
+      var emp;
+      var promise = new Promise((resolve, reject) => {
+        
+         for (var i=0; i < employees.length; i++){
+             if (employees[i].employeeNum == num) {
+                 emp = employees[i];
+                 i = employees.length;
+             }
+         } 
+         if(emp === "undefined") {
+          var err = "getEmployeesByNum() Not Filled.";
+          reject({message: err});
+         }  
+      resolve (emp);
+      })
+      return promise;
+  };
+//*****************************************************************/
+
 exports.initialize = function () {
     var promise = new Promise((resolve, reject) => {
         try {
--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 /*********************************************************************************
-* WEB322 – Assignment 02
+* WEB322 – Assignment 03
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part 
 * of this assignment has been copied manually or electronically from any other source 
 * (including 3rd party web sites) or distributed to other students.
 * 
-* Name: __Sahar Hosseinichegeni____________________ Student ID: _139670202___ Date: _21-10-10___
+* Name: __Sahar Hosseinichegeni____________________ Student ID: _139670202___ Date: _21-10-24___
 *
-* Online (Heroku) Link: ________________________________________________________
-*
+* Online (Heroku) Link: 
 ********************************************************************************/
 
-var express = require("express");
+const express = require("express");
 const path = require("path");
+const multer = require("multer");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 var dataService = require("./data-service");
-
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('./public/')); 
 
 var HTTP_PORT = process.env.PORT || 8080;
 
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 };
-
-app.use(express.static('public')); 
 
 app.get("/", function(req,res){
   res.sendFile(path.join(__dirname,"/views/home.html"));
@@ -32,20 +34,103 @@ app.get("/about", function(req,res){
   res.sendFile(path.join(__dirname,"/views/about.html"));
 });
 
-//********************************************************** */
-  app.get("/employees", function(req,res){
-    dataService.getAllEmployees()
+app.get("/employees/add", function(req,res){
+  res.sendFile(path.join(__dirname,"/views/addEmployee.html"));
+});
+
+app.get("/images/add", function(req,res){
+  res.sendFile(path.join(__dirname,"/views/addImage.html"));
+});
+
+app.post("/employees/add", function (req, res) {
+  dataService.addEmployee(req.body)
+    .then(() => {
+      res.redirect("/employees");
+    });
+});
+
+app.get("/employees", function (req, res) {
+  if (req.query.status) {
+    dataService.getEmployeesByStatus(req.query.status)
       .then((data) => {
-        console.log ("Employees JSON file");
         res.json(data);
       })
       .catch((err) => {
-        console.log(err);
         res.json(err);
       })
+  }
+  else
+    if (req.query.department) {
+      dataService.getEmployeesByDepartment(req.query.department)
+        .then((data) => {
+          res.json(data);
+        })
+        .catch((err) => {
+          res.json(err);
+        })
+    }
+    else 
+    if (req.query.isManager) {
+      dataService.getEmployeesByManager(req.query.isManager)
+        .then((data) => {
+          res.json(data);
+        })
+        .catch((err) => {
+          res.json(err);
+        })
+    }
+    else {
+      dataService.getAllEmployees()
+        .then((data) => {
+          console.log("getAllEmployees JSON.");
+          res.json(data);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.json(err);
+        })
+    }
+});
+
+app.get("/employees/:num", function (req, res) {
+  dataService.getEmployeeByNum(req.params.num)
+  .then((data) => {
+    res.json(data);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.json(err);
+  })
+});
+/********************************* Image Handler **********************************/
+//**********************************************************************************/
+const storage = multer.diskStorage({
+  destination: "./public/images/uploaded",
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
+
+app.post("/images/add", upload.single("imageFile"), function (req, res) {
+  res.redirect("/images");
+});
+
+app.get("/images", function (req, res) {
+  var imagePath = '/public/images/uploaded'; 
+  fs.readdir(path.join(__dirname, imagePath), function (err, items) {
+    var imagesContent = { images: [] };
+    var size = items.length;
+    for (var i = 0; i < items.length; i++) {
+      imagesContent.images.push(items[i]);
+    }
+    res.json(imagesContent);
   });
 
-//************************************************************/
+});
+/********************************* End Of Image Handler ********************************/
+//***************************************************************************************/
+
 app.get("/managers", function(req,res){
   dataService.getManagers()
     .then((data) => {
